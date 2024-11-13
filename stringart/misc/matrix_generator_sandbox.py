862e77f55289dd6a1228af24a75727af114d9686
Adrian Mincu <adrian-lucian.mincu@s.unibuc.ro>
2024-11-13 09:35:17 +0200

    produced first image with stringart

--- stringart/misc/matrix_generator_sandbox.py
+++ stringart/misc/matrix_generator_sandbox.py
@@ -1,27 +1,35 @@
 import numpy as np
 import scipy
+from matplotlib import pyplot as plt
+from skimage import io
 from stringart.line_algorithms.matrix import DenseMatrixGenerator
 from stringart.utils.image import ImageWrapper
 
 image = ImageWrapper()
-image.read_bw("../../imgs/lenna.png")
+image.read_bw("../../imgs/lena.png")
 b = image.flatten_image()
 
 shape = image.get_shape()
-A, pegs, lines = DenseMatrixGenerator.compute_matrix(shape, 10)
+A, pegs, lines = DenseMatrixGenerator.compute_matrix(shape, 100)
 
 first_line = A[:, 2]
 matrix = np.reshape(first_line, shape=shape)
 pegs_arr = [[point.y, point.x] for point in pegs]
 
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        if [i, j] in pegs_arr:
-            print("*", end=" ")
-            continue
-        print("." if matrix[i][j] == 0 else "#", end=" ")
-    print()
+# for i in range(len(matrix)):
+#     for j in range(len(matrix[i])):
+#         if [i, j] in pegs_arr:
+#             print("*", end=" ")
+#             continue
+#         print("." if matrix[i][j] == 0 else "#", end=" ")
+#     print()
 
 x, _, _, _ = scipy.linalg.lstsq(A, b)
 
-print(x)
+solution = np.dot(A, x)
+solution = np.clip(np.reshape(solution, shape=shape), a_min=0, a_max=1)
+solution = np.multiply(solution, 255).astype(np.uint8)
+
+io.imsave("../../outputs/lena_stringart.png", solution)
+io.imshow(solution)
+plt.show()
