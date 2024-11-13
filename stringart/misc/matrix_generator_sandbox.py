81b4ed00822a6013b988f24f1254cfeed51a5ea1
Adrian Mincu <adrian-lucian.mincu@s.unibuc.ro>
2024-11-13 09:15:13 +0200

    loaded image and solved system

--- stringart/misc/matrix_generator_sandbox.py
+++ stringart/misc/matrix_generator_sandbox.py
@@ -1,10 +1,17 @@
 import numpy as np
+import scipy
 from stringart.line_algorithms.matrix import DenseMatrixGenerator
+from stringart.utils.image import ImageWrapper
 
-A, pegs, lines = DenseMatrixGenerator.compute_matrix((20, 20), 10)
+image = ImageWrapper()
+image.read_bw("../../imgs/lenna.png")
+b = image.flatten_image()
+
+shape = image.get_shape()
+A, pegs, lines = DenseMatrixGenerator.compute_matrix(shape, 10)
 
 first_line = A[:, 2]
-matrix = np.reshape(first_line, shape=(20, 20))
+matrix = np.reshape(first_line, shape=shape)
 pegs_arr = [[point.y, point.x] for point in pegs]
 
 for i in range(len(matrix)):
@@ -14,3 +21,7 @@ for i in range(len(matrix)):
             continue
         print("." if matrix[i][j] == 0 else "#", end=" ")
     print()
+
+x, _, _, _ = scipy.linalg.lstsq(A, b)
+
+print(x)
