32c8b5ea0864d48056fd8cccbf9744a6ac5d9151
skpha13 <mal13adi03@gmail.com>
2025-06-24 15:57:26 +0300

    feat: gif animation

---
+++ stringart/misc/demo_animation.py
@@ -0,0 +1,52 @@
+import os
+from pathlib import Path
+
+import imageio
+import numpy as np
+from stringart.optimize.downsampling import UDSLoss
+from stringart.utils.image import ImageWrapper
+from stringart.utils.perf_analyzer import Benchmark, BenchmarkResult
+from tqdm import tqdm
+
+
+def animation(x: np.ndarray, loss: UDSLoss, filename: str = "animation.gif"):
+    x_temp = np.zeros(x.shape)
+    frames = []
+    indices = np.flatnonzero(x)
+
+    for i in tqdm(indices, desc="Drawing lines"):
+        x_temp[i] = 1
+        _, image = loss(x_temp)
+
+        # rgb dimension conversion from grayscale
+        if image.ndim == 2:
+            image = np.stack([image] * 3, axis=-1)
+
+        frames.append(image)
+
+    # to last exactly one minute
+    duration = 60 / len(frames)
+    imageio.mimsave(filename, frames, duration=duration)
+
+
+def generate_animations(xs: list[np.ndarray], loss: UDSLoss):
+    for i, x in enumerate(xs):
+        animation(x, loss, filename=f"../../outputs/demo/animation_{i}.gif")
+
+
+if __name__ == "__main__":
+    stringart_directory: Path = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    directory: Path = stringart_directory.parent.resolve()
+    Benchmark.initialize_metadata(directory)
+
+    results: list[BenchmarkResult] = Benchmark.load_benchmarks("radon_quantitative")
+    image = ImageWrapper.scale_image(results[0].output_image)
+    number_of_pegs = results[0].number_of_pegs
+    crop_mode = results[0].crop_mode
+    rasterization = results[0].rasterization
+    block_size = results[0].block_size
+
+    xs = [np.array(results[0].x), np.array(results[1].x), np.array(results[2].x)]
+    loss = UDSLoss(image, crop_mode, number_of_pegs, rasterization, block_size)
+
+    generate_animations(xs, loss)
